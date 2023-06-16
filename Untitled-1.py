c5f9c374b28bfe1f57d9c2ecd7d9c27e3323e061
Theuzindeveloper <131308267+Matheusleitelima@users.noreply.github.com>
2023-06-15 22:20:47 -0300

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,54 @@
+nome = input('qual seu nome?')
+idade = input('quual asua idade?')
+peso = input('qual é seu peso?')
+print(nome , idade , peso)
+
+
+#tarefa 1#
+
+
+nome =input('qual é seu nome?')
+print('olá' , nome  ,'!', 'prazer em te conhecer!')
+
+
+#tarefa 02#
+
+dia = input(f'que dia você nasceu?')
+mês = input (f'que mês você nasceu?')
+ano = input(f'que ano você nasceu?')
+print(f'você nasceu no dia' , dia ,'de' , mês , 'de' , ano , '.','Correto?')
+
+
+#tipos primitivos
+
+int = 7,-4,0,9875
+
+float = 4.5, 0.076 , 15.223 , 7.0
+
+bool = true ,  false 
+
+str = 'Olá' , '7.5'
+
+#tarefa 03#
+
+numero1 =int(input('dgite o primeiro :'))
+numero2 = int(input('digite um numero:'))
+soma = numero1 + numero2
+print(f' {numero1} + {numero2} é {soma}:')
+
+
+#exemplo 2
+
+
+numero1 =int(input('dgite o primeiro :'))
+numero2 = int(input('digite um numero:'))
+soma = numero1 + numero2
+ #print(f'a soma entre' , numero1 , 'e' , numero2 , 'é igual a' , soma )
+
+print('a soma entre {} e {} vale {}' .format(numero1 , numero2 , soma))
+
+
+
+
+
+
